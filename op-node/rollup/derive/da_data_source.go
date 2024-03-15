b90669aebb5d45c72a9a09229c8af752a6d3b8d7
Jiankun Yang <110372666+0xyjk@users.noreply.github.com>
2024-03-16 01:09:49 +0800

    fix: da data source

--- op-node/rollup/derive/da_data_source.go
+++ op-node/rollup/derive/da_data_source.go
@@ -38,15 +38,17 @@ func (s *DaDataSource) Next(ctx context.Context) (eth.Data, error) {
 		}
 	}
 
+	var data eth.Data
 	c := calldata.Calldata{}
 	if err := proto.Unmarshal(s.comm, &c); err != nil {
 		log.Warn("tx data is not a protobuf calldata, probably raw data", "txDataHex", hex.EncodeToString(s.comm), "block", s.id.Number)
-		return s.comm, nil
-	}
-	data, err := da.Get(context.Background(), s.log, &c)
-	if err != nil {
-		log.Error("failed to retrieve data from DA", "err", err, "block", s.id.Number)
-		return nil, NewTemporaryError(fmt.Errorf("failed to retrieve data from DA: %v", err))
+		data = s.comm
+	} else {
+		data, err = da.Get(context.Background(), s.log, &c)
+		if err != nil {
+			log.Error("failed to retrieve data from DA", "err", err, "block", s.id.Number)
+			return nil, NewTemporaryError(fmt.Errorf("failed to retrieve data from DA: %v", err))
+		}
 	}
 
 	// reset the commitment so we can fetch the next one from the source at the next iteration.
