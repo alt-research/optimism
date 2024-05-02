0456b8162a46dc6b5090894c875fd92c8e9fd181
Aten Jin <jincxmain@gmail.com>
2024-05-02 23:25:51 +0800

    use origin finality logic

--- op-node/rollup/derive/pipeline.go
+++ op-node/rollup/derive/pipeline.go
@@ -127,11 +127,11 @@ func (dp *DerivationPipeline) Origin() eth.L1BlockRef {
 func (dp *DerivationPipeline) Finalize(l1Origin eth.L1BlockRef) {
 	// In plasma mode, the finalization signal is proxied through the plasma manager.
 	// Finality signal will come from the DA contract or L1 finality whichever is last.
-	if dp.rollupCfg.UsePlasma {
-		dp.plasma.Finalize(l1Origin)
-	} else {
-		dp.eng.Finalize(l1Origin)
-	}
+	//if dp.rollupCfg.UsePlasma {
+	//	dp.plasma.Finalize(l1Origin)
+	//} else {
+	dp.eng.Finalize(l1Origin)
+	//}
 }
 
 // FinalizedL1 is the L1 finalization of the inner-most stage of the derivation pipeline,
