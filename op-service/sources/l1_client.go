6d6e3f27d8561557dd5fc291ed131ac98a3faf3f
fyInALT <fy@altresear.ch>
2024-08-16 11:21:14 +0800

    cache for fast req the l1 datas

--- op-service/sources/l1_client.go
+++ op-service/sources/l1_client.go
@@ -3,6 +3,8 @@ package sources
 import (
 	"context"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -30,9 +32,21 @@ func L1ClientDefaultConfig(config *rollup.Config, trustRPC bool, kind RPCProvide
 
 func L1ClientSimpleConfig(trustRPC bool, kind RPCProviderKind, cacheSize int) *L1ClientConfig {
 	span := cacheSize
-	if span > 1000 { // sanity cap. If a large sequencing window is configured, do not make the cache too large
-		span = 1000
+	if span > 10000 { // sanity cap. If a large sequencing window is configured, do not make the cache too large
+		span = 10000
 	}
+
+	{
+		envVar := os.Getenv("CACHE_SIZE")
+		if len(envVar) != 0 {
+			var err error
+			span, err = strconv.Atoi(envVar)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
 	return &L1ClientConfig{
 		EthClientConfig: EthClientConfig{
 			// receipts and transactions are cached per block
@@ -40,15 +54,15 @@ func L1ClientSimpleConfig(trustRPC bool, kind RPCProviderKind, cacheSize int) *L
 			TransactionsCacheSize: span,
 			HeadersCacheSize:      span,
 			PayloadsCacheSize:     span,
-			MaxRequestsPerBatch:   20, // TODO: tune batch param
-			MaxConcurrentRequests: 10,
+			MaxRequestsPerBatch:   200, // TODO: tune batch param
+			MaxConcurrentRequests: 100,
 			TrustRPC:              trustRPC,
 			MustBePostMerge:       false,
 			RPCProviderKind:       kind,
 			MethodResetDuration:   time.Minute,
 		},
 		// Not bounded by span, to cover find-sync-start range fully for speedy recovery after errors.
-		L1BlockRefsCacheSize: cacheSize,
+		L1BlockRefsCacheSize: span,
 	}
 }
 
