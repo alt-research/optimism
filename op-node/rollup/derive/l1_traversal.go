232ddcc1fc6ff1c925f75c5fae5e6582fbd01aec
fyInALT <fy@altresear.ch>
2024-08-16 20:59:26 +0800

    add CONFIG_UPDATE_BLOCKS env path

--- op-node/rollup/derive/l1_traversal.go
+++ op-node/rollup/derive/l1_traversal.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
+	"strings"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
@@ -29,15 +31,36 @@ type L1Traversal struct {
 	log      log.Logger
 	sysCfg   eth.SystemConfig
 	cfg      *rollup.Config
+
+	configUpgradeBlocksMap map[string]bool
 }
 
 var _ ResettableStage = (*L1Traversal)(nil)
 
 func NewL1Traversal(log log.Logger, cfg *rollup.Config, l1Blocks L1BlockRefByNumberFetcher) *L1Traversal {
+	configUpgradeBlocksMap := make(map[string]bool)
+
+	envVar := os.Getenv("CONFIG_UPDATE_BLOCKS")
+	if len(envVar) != 0 {
+		configUpgradeBlocks := strings.Split(envVar, ",")
+		for _, blk := range configUpgradeBlocks {
+			log.Warn("Use of configUpgradeBlocks", "hash", blk)
+			configUpgradeBlocksMap[blk] = true
+		}
+	} else {
+		log.Warn("No CONFIG_UPDATE_BLOCKS found")
+	}
+
+	// TODO: for xertio, different block hash between different block chain
+	// so just add
+	configUpgradeBlocksMap["0x1405620b142fbda295e314e79eeec1162493ee42aebd9bbe77b50bf277e46e4a"] = true
+
 	return &L1Traversal{
 		log:      log,
 		l1Blocks: l1Blocks,
 		cfg:      cfg,
+
+		configUpgradeBlocksMap: configUpgradeBlocksMap,
 	}
 }
 
@@ -72,8 +95,11 @@ func (l1t *L1Traversal) AdvanceL1Block(ctx context.Context) error {
 
 	// FIXME: in all mainnet only block 37331537 the 0x1a6f69e62ecb37555ca0ea224ef891005bb222f68b52e74297fcb282d4678ad9 tx
 	// have the system config update, so we can not need fetch!
-	if nextL1Origin.Hash.Hex() == "0x1405620b142fbda295e314e79eeec1162493ee42aebd9bbe77b50bf277e46e4a" {
-		l1t.log.Warn("call nextL1Origin")
+	_, isInMap := l1t.configUpgradeBlocksMap[nextL1Origin.Hash.Hex()]
+	if isInMap {
+		l1t.log.Warn("call nextL1Origin",
+			"hash", nextL1Origin.Hash.Hex(),
+			"number", nextL1Origin.Number)
 		// Parse L1 receipts of the given block and update the L1 system configuration
 		_, receipts, err := l1t.l1Blocks.FetchReceipts(ctx, nextL1Origin.Hash)
 		if err != nil {
