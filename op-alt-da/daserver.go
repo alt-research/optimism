52500ead409a6ef8b3154de63edc74e8ff83eced
Jiankun Yang <110372666+0xyjk@users.noreply.github.com>
2025-01-13 21:44:17 +0800

    feat: support xterio commitment
    fix: build
    
    fix build
    
    add command
    
    fix: verify commitment
    
    remove check for `CommitmentType`
    
    fix commitment for XterioCommitment
    
    allow XterioCommitment raw type
    
    add log
    
    remove limit for `--max-l1-tx-size-bytes` for alt-da

--- op-alt-da/daserver.go
+++ op-alt-da/daserver.go
@@ -34,9 +34,10 @@ type DAServer struct {
 	httpServer     *http.Server
 	listener       net.Listener
 	useGenericComm bool
+	useXterioComm  bool
 }
 
-func NewDAServer(host string, port int, store KVStore, log log.Logger, useGenericComm bool) *DAServer {
+func NewDAServer(host string, port int, store KVStore, log log.Logger, useGenericComm, useXterioComm bool) *DAServer {
 	endpoint := net.JoinHostPort(host, strconv.Itoa(port))
 	return &DAServer{
 		log:      log,
@@ -46,6 +47,7 @@ func NewDAServer(host string, port int, store KVStore, log log.Logger, useGeneri
 			Addr: endpoint,
 		},
 		useGenericComm: useGenericComm,
+		useXterioComm:  useXterioComm,
 	}
 }
 
@@ -143,19 +145,25 @@ func (d *DAServer) HandlePut(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path == "/put" || r.URL.Path == "/put/" { // without commitment
 		var comm []byte
-		if d.useGenericComm {
-			n, err := rand.Int(rand.Reader, big.NewInt(99999999999999))
-			if err != nil {
-				d.log.Error("Failed to generate commitment", "err", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			comm = append(comm, 0x01)
-			comm = append(comm, 0xff)
-			comm = append(comm, n.Bytes()...)
-
+		var srcComm XterioCommitment
+		if d.useXterioComm {
+			srcComm = NewXterioCommitmentData(input)
+			comm = srcComm.Encode()
 		} else {
-			comm = NewKeccak256Commitment(input).Encode()
+			if d.useGenericComm {
+				n, err := rand.Int(rand.Reader, big.NewInt(99999999999999))
+				if err != nil {
+					d.log.Error("Failed to generate commitment", "err", err)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				comm = append(comm, 0x01)
+				comm = append(comm, 0xff)
+				comm = append(comm, n.Bytes()...)
+
+			} else {
+				comm = NewKeccak256Commitment(input).Encode()
+			}
 		}
 
 		if err = d.store.Put(r.Context(), comm, input); err != nil {
@@ -164,6 +172,12 @@ func (d *DAServer) HandlePut(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		d.log.Info("stored commitment", "key", hex.EncodeToString(comm), "input_len", len(input))
+
+		if d.useXterioComm {
+			// response comm for xterio type should use `srcComm` rather than `comm`
+			comm = srcComm
+			d.log.Info("response comm for xterio commitment", "key", hex.EncodeToString(comm), "input_len", len(input))
+		}
 
 		if _, err := w.Write(comm); err != nil {
 			d.log.Error("Failed to write commitment request body", "err", err, "comm", comm)
--- op-node/rollup/derive/altda_data_source.go
+++ op-node/rollup/derive/altda_data_source.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 
 	altda "github.com/ethereum-optimism/optimism/op-alt-da"
+	"github.com/ethereum-optimism/optimism/op-alt-da/pb/xterio"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/derive/params"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/ethereum/go-ethereum/log"
+	"google.golang.org/protobuf/proto"
 )
 
 // AltDADataSource is a data source that fetches inputs from a AltDA provider given
@@ -55,20 +57,33 @@ func (s *AltDADataSource) Next(ctx context.Context) (eth.Data, error) {
 		if len(data) == 0 {
 			return nil, NotEnoughData
 		}
-		// If the tx data type is not altDA, we forward it downstream to let the next
-		// steps validate and potentially parse it as L1 DA inputs.
-		if data[0] != params.DerivationVersion1 {
-			return data, nil
-		}
+		x := &xterio.XterioCommitment{}
+		// decide whether this data is xterio type commitment
+		if err := proto.Unmarshal(data, x); err == nil {
+			// xterio commitment allow Digest/Raw type. if it's Raw, means this is the raw data for da data, return with
+			// the raw data rather than try to access the da server.
+			// Notice it must before `s.comm` for `s.comm` should be nil at here, can not set a value for it.
+			if x, ok := x.GetValue().(*xterio.XterioCommitment_Raw); ok {
+				s.log.Info("This commitment is XterioCommitment Raw type, return data directly rather than get data from da server.", "raw", data[0:16])
+				return x.Raw, nil
+			}
+			s.comm = altda.XterioCommitment(data)
+		} else {
+			// If the tx data type is not altDA, we forward it downstream to let the next
+			// steps validate and potentially parse it as L1 DA inputs.
+			if data[0] != params.DerivationVersion1 {
+				return data, nil
+			}
 
-		// validate batcher inbox data is a commitment.
-		// strip the transaction data version byte from the data before decoding.
-		comm, err := altda.DecodeCommitmentData(data[1:])
-		if err != nil {
-			s.log.Warn("invalid commitment", "commitment", data, "err", err)
-			return nil, NotEnoughData
+			// validate batcher inbox data is a commitment.
+			// strip the transaction data version byte from the data before decoding.
+			comm, err := altda.DecodeCommitmentData(data[1:])
+			if err != nil {
+				s.log.Warn("invalid commitment", "commitment", data, "err", err)
+				return nil, NotEnoughData
+			}
+			s.comm = comm
 		}
-		s.comm = comm
 	}
 	// use the commitment to fetch the input from the AltDA provider.
 	data, err := s.fetcher.GetInput(ctx, s.l1, s.comm, s.id)
