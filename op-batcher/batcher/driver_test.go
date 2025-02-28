a88a279b6d7a9c49dadcbeff819b83066c22da31
Aten Jin <jincxmain@gmail.com>
2025-02-28 17:54:12 +0800

    Revert "op-batcher: use local-safe to reduce lag during interop sync (#14265)"
    This reverts commit 56b7f49d96a2013656bfff1d71923147af25ed46.

--- op-batcher/batcher/driver_test.go
+++ op-batcher/batcher/driver_test.go
@@ -90,7 +90,7 @@ func TestBatchSubmitter_SafeL1Origin(t *testing.T) {
 				ep.rollupClient.ExpectSyncStatus(&eth.SyncStatus{}, errors.New("failed to fetch sync status"))
 			} else {
 				ep.rollupClient.ExpectSyncStatus(&eth.SyncStatus{
-					LocalSafeL2: eth.L2BlockRef{
+					SafeL2: eth.L2BlockRef{
 						L1Origin: eth.BlockID{
 							Number: tt.currentSafeOrigin,
 						},
