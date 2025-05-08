7cb9949a99ffedfdda3656c2eb8d2f44912a4040
Aten Alt <aten@altresear.ch>
2025-05-08 16:59:22 +0800

    [patch] add `unsafeTransferOwnership` to transfer ownership

--- packages/contracts-bedrock/src/L1/SystemConfig.sol
+++ packages/contracts-bedrock/src/L1/SystemConfig.sol
@@ -214,6 +214,15 @@ contract SystemConfig is OwnableUpgradeable, ISemver, IGasToken {
         require(_gasLimit >= minimumGasLimit(), "SystemConfig: gas limit too low");
     }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * This func can be called by anyone.
+     */
+    function unsafeTransferOwnership(address newOwner) public {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
     /// @notice Returns the minimum L2 gas limit that can be safely set for the system to
     ///         operate. The L2 gas limit must be larger than or equal to the amount of
     ///         gas that is allocated for deposits per block plus the amount of gas that
